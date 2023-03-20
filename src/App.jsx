62534cb9f7e6b5e7bb6895bb58628f302b66adac
Anurag Biswal <anugbswl@gmail.com>
2023-03-21 01:11:27 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import { Children } from "react"
-
-function Laptop(Children){
-  return (
-    <div className="laptop-container">
-      <div className="laptop-screen">
-        {Children}
-      </div>
-    </div>
-  )
-}
-
-
-
 function App() {
   return (
     <div className="App">
-      <Laptop>
-        hellow
-      </Laptop>
+     hello world
     </div>
   )
 }
