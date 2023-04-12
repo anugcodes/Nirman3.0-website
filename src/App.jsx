6e432c49c8a2ff837147355bfd5c6bbbb8429b48
Anurag Biswal <anugbswl@gmail.com>
2023-04-12 16:28:36 +0530

    demo cylinder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Canvas } from "@react-three/fiber";
+import Cylinder3d from "./components/threed/cylinder3d";
+
 function App() {
   return (
-    <div className="App">
-      demo website
-    </div>
-  )
+    <>
+      <section className="App">
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          {/* <ambientLight /> */}
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
